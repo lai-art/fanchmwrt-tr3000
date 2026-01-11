bcc6cfd605a0de751d71d9c6baac9859a381b160
VisinSky <visin@live.cn>
2026-01-11 14:27:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,6 @@
 
 # Add a feed source
 # echo 'src-git filebrowser https://github.com/kiddin9/openwrt-filebrowser.git' >>feeds.conf.default
-# echo 'src-git timedreboot https://github.com/kiddin9/luci-app-timedreboot.git' >>feeds.conf.default
 echo 'src-git gecoosac https://github.com/lwb1978/openwrt-gecoosac.git' >>feeds.conf.default
 echo 'src-git ddnsgo  https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf.default
 echo 'src-git watchdog https://github.com/sirpdboy/luci-app-watchdog' >>feeds.conf.default
@@ -23,4 +22,5 @@ echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >>feeds.conf.d
 git clone https://github.com/DustReliant/luci-app-filetransfer.git package/luci-app-filetransfer
 git clone https://github.com/timsaya/openwrt-bandix.git package/openwrt-bandix
 git clone https://github.com/timsaya/luci-app-bandix.git package/luci-app-bandix
+git clone https://github.com/kiddin9/luci-app-timedreboot.git package/luci-app-timedreboot
 # echo 'src-git kwrtpackages https://github.com/kiddin9/kwrt-packages.git' >>feeds.conf.default
